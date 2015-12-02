ce3bac7283fb6b6c9a3a2cd09c1401c9c58068e0
Frank Bergmann <frank.bergmann@project-open.com>
2015-12-02 19:34:17 +0000

    - Beautified comments

--- sql/oracle/intranet-payments-backup.sql
+++ sql/oracle/intranet-payments-backup.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-payments/sql/oracle/intranet-payments-backup.sql
 --
--- Copyright (C) 2004 Project/Open
+-- Copyright (C) 2004 ]project-open[
 --
 -- This program is free software. You can redistribute it
 -- and/or modify it under the terms of the GNU General
